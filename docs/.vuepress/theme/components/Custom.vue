e1f4b5f0706a7ffff4ded5e2e8787bdbeef642be
Ling-Ink <clovermoling@gmail.com>
2025-08-11 20:57:44 +0800

    feat: Add Google Tag Manager

--- docs/.vuepress/theme/components/Custom.vue
+++ docs/.vuepress/theme/components/Custom.vue
@@ -5,6 +5,11 @@ const message = ref('Hello World!')
 </script>
 
 <template>
+  <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ3FP3K3"
+    height="0" width="0" style="display:none;visibility:hidden">
+    </iframe>
+  </noscript>
   <div class="my-custom-content">
     {{ message }}
   </div>
